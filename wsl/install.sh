922ba3dbde769db1b52ccd95dfd64e690d4710a0
Michal Nestorowicz <michal.nestorowicz@gmail.com>
2023-06-23 21:56:37 +0200

    add color to install.sh

--- wsl/install.sh
+++ wsl/install.sh
@@ -3,27 +3,38 @@
 set -e
 
 ZSHRC="${HOME}/.zshrc"
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-if ! command -v zsh &> /dev/null
-then
-  echo "installing zsh"
+log () {
+  echo -e "${GREEN}${1}${NC}\n"
+}
+
+if ! command -v wslview &> /dev/null ; then
+  log "installing wslu"
+  sudo apt -y install wslu
+fi
+
+if ! command -v zsh &> /dev/null ; then
+  log "installing zsh"
   sudo apt-get -y install zsh
 fi
 
-echo "copying configs"
+log "copying configs"
 cp $PWD/.zshrc $HOME/
 cp -r $PWD/.zsh $PWD/.fzf $HOME/
 
 if ! command -v fzf &> /dev/null ; then
-  echo "installing fzf"
+  log "installing fzf"
   $HOME/.fzf/install --key-bindings --completion --update-rc
 fi
 
 if ! [ -s "$NVM_DIR/nvm.sh" ] ; then
-  echo "installing nvm"
+  log "installing nvm"
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 fi
 
 echo '' >> ${ZSHRC}
 echo '# zsh-syntax-highlighting has to be the last statement as per documentation' >> ${ZSHRC}
 echo 'source ${ZSH}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' >> ${ZSHRC}
+
